27d334f9c18399b8cdd672f9c46df783d19250d8
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro-8.local>
2022-10-25 21:50:29 -0400

    change few thing

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function closeNav() {
 }
 
 const image1 = new Image();
-//image1.src = 'pexels-photo-1108099.jpeg';
-//image1.src = '5616.jpg';
 var keys = document.getElementById("key");
 var button= document.getElementById("sb");
-function searchfun(){
-  var xhr = new XMLHttpRequest();       
+//for url submit
+if(button){
+  button.addEventListener('click', function(){
+    var xhr = new XMLHttpRequest();       
     xhr.open("GET", keys.value, true); 
     xhr.responseType = "blob";
     xhr.onload = function (e) {
@@ -28,13 +28,11 @@ function searchfun(){
       reader.readAsDataURL(file);
     };
     xhr.send()
- 
-}
-if(button){
-button.addEventListener('click', searchfun);
+  });
 }
 const image_input = document.getElementById("image_input");
 var uploaded_image = "";
+//for upload option
 if(image_input){
 image_input.addEventListener("change", function(){
   const reader = new FileReader();
@@ -52,7 +50,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
 
-var s = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+var s = 'Ñ@#W$%[#!)(&^*?!;:+=-,._ ';
 const inputSlider = document.getElementById('resolution');
 const inputLabel = document.getAnimations('resoltionLabel');
 inputSlider.addEventListener('change', handleSlider);
@@ -75,73 +73,72 @@ function mapRange (value, a, b, c, d) {
   return c + value * (d - c);
 }
 class Ascii{
-  #imageCellArray = [];
-  #pixels = [];
-  #ctx;
-  #width;
-  #height;
+  imageCellArray = [];
+  pixels = [];
+  ctx;
+  width;
+  height;
   constructor(ctx, width, height){
-    this.#ctx = ctx;
-    this.#width = width;
-    this.#height = height;
-    this.#ctx.drawImage(image1, 0 ,0, this.#width, this.#height);
-    this.#pixels = this.#ctx.getImageData(0,0,this.#width, this.#height);
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.ctx.drawImage(image1, 0 ,0, this.width, this.height);
+    this.pixels = this.ctx.getImageData(0,0,this.width, this.height);
   }
-  #convertToSymbol(g){
+  convertToSymbol(g){
     if(g >250) return '@';
     else return s[(parseInt(mapRange(g, 0 , 255, s.length,0 )))];
   }
-  #scanImage(cellSize){
-    this.#imageCellArray = [];
-    for(let y = 0; y <this.#pixels.height; y+= cellSize){
-      for(let x = 0; x < this.#pixels.width; x+= cellSize){
+  scanImage(cellSize){
+    this.imageCellArray = [];
+    for(let y = 0; y <this.pixels.height; y+= cellSize){
+      for(let x = 0; x < this.pixels.width; x+= cellSize){
         //each pixels has 4 members rbgi
         const posX = x*4;
         const posY = y*4;
         //next row
-        const pos = (posY*this.#pixels.width) + posX;
+        const pos = (posY*this.pixels.width) + posX;
         //get the last value of rgbi and check transparent
-        if(this.#pixels.data[pos+3] > 128){
-          const red = this.#pixels.data[pos];
-          const green = this.#pixels.data[pos + 1];
-          const blue = this.#pixels.data[pos +2];
+        if(this.pixels.data[pos+3] > 128){
+          const red = this.pixels.data[pos];
+          const green = this.pixels.data[pos + 1];
+          const blue = this.pixels.data[pos +2];
           const total = red + green + blue;
           const averageColorValue = total/3;
           const color = "rgb(" + red + "," + green + "," + blue + ")";
-          const symbol = this.#convertToSymbol(averageColorValue);
-          if(total > 200) this.#imageCellArray.push(new Cell(x,y,symbol,color));
+          const symbol = this.convertToSymbol(averageColorValue);
+          this.imageCellArray.push(new Cell(x,y,symbol,color));
         }
       }
     }
   }
-  #drawAscii(){
-    this.#ctx.clearRect(0,0,this.#width, this.#height);
-    for(let i =0; i<this.#imageCellArray.length;i++){
-      this.#imageCellArray[i].draw(this.#ctx);
+  drawAscii(){
+    this.ctx.clearRect(0,0,this.width, this.height);
+    for(let i =0; i<this.imageCellArray.length;i++){
+      this.imageCellArray[i].draw(this.ctx);
     }
   }
   draw(cellSize){
-    this.#scanImage(cellSize);
-    this.#drawAscii();
+    this.scanImage(cellSize);
+    this.drawAscii();
   }
 }
 let effect;
 function handleSlider(){
+  //full scale
   if(inputSlider.value == 1){
-    inputLabel.innerHTML = 'Original image';
     ctx.drawImage(image1, 0 ,0, canvas.width, canvas.height);
   }
   else{
-    inputLabel.innerHTML = 'Resolution: ' + inputSlider.value + ' px';
-    ctx.font = parseInt(inputSlider.value)*1.2 +'px Verdana';
+    ctx.font = parseInt(inputSlider.value)*1.5 +'px Verdana';
     effect.draw(parseInt(inputSlider.value));
   }
 }
-image1.onload = function initialize(){
+image1.onload = function (){
   canvas.width = image1.width;
   canvas.height = image1.height;
   effect = new Ascii(ctx, canvas.width, canvas.height);
-  effect.draw(5);
+  effect.draw(2);
 }
 
 function download(){
