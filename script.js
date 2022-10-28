8cd5b284ef274d88e2d8ede21c343cbcf3854175
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro-8.local>
2022-10-27 22:36:11 -0400

    add images search tool

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -147,3 +148,7 @@ function download(){
   imageLink.href = canvas.toDataURL('image/png',1);
   imageLink.click();
 }
+
+
+
+
